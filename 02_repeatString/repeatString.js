d37bec7ea1ec3c8f5dcd30627252d78e7066d36b
aashishrupreja <aashish.rupreja4797@gmail.com>
2023-04-16 21:38:52 +0530

    Complete exercises 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+
+    let repStr = "";
+
+    if (num < 0)
+        return 'ERROR';
+
+    for (let i = 1; i <= num; i++) {
+        repStr = repStr.concat(str);
+    }
+    return repStr;
 
 };
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString; 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+
+    let revStr = "";
+
+    for (let i = str.length; i >= 0; i--) {
+        revStr += str.charAt(i);
+    }
+    return revStr;
 
 };
 
